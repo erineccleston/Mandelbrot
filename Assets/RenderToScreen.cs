55fe60647cbf620e9a1ad0894cf1f0c783d8cdb6
Erin <connoreccleston@gmail.com>
2019-10-10 00:22:54 -0600

    Initial commit

---
+++ Assets/RenderToScreen.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class RenderToScreen : MonoBehaviour
+{
+    public RenderTexture RendTex;
+    public ComputeShader Shader;
+
+    [SerializeField]
+    // left, dx, top, dy
+    float[] Params = new float[4];
+
+    public Vector3 Speed = new Vector3(1, 1, 1);
+
+    [SerializeField]
+    // a, b, r
+    Vector3 Position = new Vector3(0, 0, 2);
+    Vector3 LastPosition;
+
+    void Update()
+    {
+        int GetValue(KeyCode negative, KeyCode positive)
+        {
+            return (Input.GetKey(negative) ? -1 : 0) + (Input.GetKey(positive) ? 1 : 0);
+        }
+
+        float a, b, r;
+        a = GetValue(KeyCode.A, KeyCode.D) * Time.deltaTime * Speed.x;
+        b = GetValue(KeyCode.S, KeyCode.W) * Time.deltaTime * Speed.y;
+        r = GetValue(KeyCode.E, KeyCode.Q) * Time.deltaTime * Speed.z * Position.z;
+        Position += new Vector3(a, b, r);
+    }
+
+    void UpdateParams()
+    {
+        float r1 = Position.z;
+        float r2 = Position.z * (Screen.width / Screen.height);
+
+        Params[0] = Position.x - r2;
+        Params[1] = r2 * 2 / Screen.width;
+        Params[2] = Position.y + r1;
+        Params[3] = r1 * 2 / Screen.height; ;
+    }
+
+    void Awake()
+    {
+        //RendTex = new RenderTexture(Screen.width, Screen.height, 0);
+        //RendTex.enableRandomWrite = true;
+        Shader.SetTexture(0, nameof(RendTex), RendTex);
+    }
+
+    //void OnRenderImage(RenderTexture src, RenderTexture dest)
+    //{
+    //    Graphics.Blit(RendTex, null as RenderTexture);
+    //    if (Position != LastPosition)
+    //    {
+    //        UpdateParams();
+    //        Shader.SetFloats(nameof(Params), Params);
+    //        Shader.Dispatch(Shader.FindKernel("CSMain"), Screen.width / 16, Screen.height / 16, 1);
+    //        LastPosition = Position;
+    //    }
+    //}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        //if (Position != LastPosition)
+        //{
+            var rt = RenderTexture.GetTemporary(src.width, src.height, 0);
+        rt.enableRandomWrite = true;
+            UpdateParams();
+            Shader.SetFloats(nameof(Params), Params);
+            Shader.Dispatch(0, Screen.width / 16, Screen.height / 16, 1);
+            LastPosition = Position;
+        //}
+        Graphics.Blit(RendTex, null as RenderTexture);
+            RenderTexture.ReleaseTemporary(rt);
+    }
+
+    public void Dispatch(RenderTexture texture)
+    {
+        var tmpSource = RenderTexture.GetTemporary(texture.width, texture.height, 0, texture.format);
+        Graphics.Blit(texture, tmpSource);
+
+        texture.enableRandomWrite = true;
+
+        Shader.SetTexture(0, "_Texture", tmpSource);
+        Shader.SetTexture(0, "_Result", texture);
+        Shader.Dispatch(0, texture.width / 32, texture.height / 32, 1);
+        RenderTexture.ReleaseTemporary(tmpSource);
+
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,115 @@
+﻿// https://en.wikibooks.org/wiki/Cg_Programming/Unity/Computing_Image_Effects
+
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Test : MonoBehaviour
+{
+    // todo: switch between float and double as needed
+    public ComputeShader Shader;
+
+    public int Iterations = 256;
+
+    public Vector3 Speed = Vector3.one;
+
+    static readonly Vector3 Default = new Vector3(-0.75f, 0, 1);
+
+    public Vector3 ABR = Default;
+    private Vector3 Last;
+
+    private RenderTexture Temp;
+
+    void Awake()
+    {
+        Cursor.visible = false;
+        Shader.SetInt("Iterations", Iterations);
+    }
+
+    void Update()
+    {
+        int GetValue(KeyCode negative, KeyCode positive)
+        {
+            return (Input.GetKey(negative) ? -1 : 0) + (Input.GetKey(positive) ? 1 : 0);
+        }
+
+        float a, b, r;
+        float scale = Time.deltaTime * ABR.z;
+        a = GetValue(KeyCode.A, KeyCode.D) * Speed.x * scale;
+        b = GetValue(KeyCode.S, KeyCode.W) * Speed.y * scale;
+        r = GetValue(KeyCode.E, KeyCode.Q) * Speed.z * scale;
+        ABR += new Vector3(a, b, r);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Iterations *= 2;
+            Last = Vector3.zero;
+            Shader.SetInt("Iterations", Iterations);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Iterations /= 2;
+            Last = Vector3.zero;
+            Shader.SetInt("Iterations", Iterations);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ABR = Default;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (Temp != null)
+        {
+            Temp.Release();
+            Temp = null;
+        }
+    }
+
+    Vector4 CalcParams(float width, float height)
+    {
+        float r1 = ABR.z;
+        float r2 = ABR.z * (width / height);
+
+        return new Vector4
+        (
+            ABR.x - r2,
+            r2 * 2 / width,
+            ABR.y - r1,
+            r1 * 2 / height
+        );
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        int width = src.width;
+        int height = src.height;
+
+        if (Temp == null || Temp.width != width || Temp.height != height)
+        {
+            if (Temp != null)
+                Temp.Release();
+
+            Temp = new RenderTexture(width, height, src.depth);
+            Temp.enableRandomWrite = true;
+            Temp.Create();
+            Shader.SetTexture(0, "RendTex", Temp);
+        }
+
+        if (ABR.x != Last.x || ABR.y != Last.y || ABR.z != Last.z)
+        {
+            Shader.SetVector("Parameters", CalcParams(width, height));
+            Shader.Dispatch(0, width / 32, height / 32, 1);
+            Last = ABR;
+        }
+
+        Graphics.Blit(Temp, null as RenderTexture); // test null vs dst
+    }
+}
