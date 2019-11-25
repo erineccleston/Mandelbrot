bfeb8285e0aef8dd11cda17f03fe10a96694901f
Erin <connoreccleston@gmail.com>
2019-11-25 13:05:14 -0700

    Started on 3d

---
+++ Assets/QuatTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuatTest : MonoBehaviour
+{
+    public Quaternion a, b, c, d;
+
+    void OnValidate()
+    {
+        c = a * b;
+        d = b * a;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
