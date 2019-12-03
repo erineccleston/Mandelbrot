39bfafd67e738e1038fc59a73e956358981364a2
Erin Eccleston <erineccleston96@gmail.com>
2019-12-03 14:39:16 -0700

    Started bulb using 3d tex.

---
+++ Assets/Bulb.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulb : MonoBehaviour
+{
+    public ComputeShader Shader;
+    public Vector3Int Voxels;
+    public Vector3 Begin, End;
+    public double W;
+    public int Iterations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshFilter>().mesh = new Mandelbulb(Shader, Voxels, Begin, End, W, Iterations).Mesh;
+    }
+}
