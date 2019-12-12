d54cd64098cc09be5b4e3186e78a1a7166ab0d52
DerekPaniagua <paniaguaderek@gmail.com>
2019-12-11 18:18:01 -0700

    Edited main menu and added fractal selection script

---
+++ Assets/AlgorithmSelection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlgorithmSelection : MonoBehaviour
+{
+    Dropdown drop;
+
+    private void Start()
+    {
+        drop = GetComponentInChildren<Dropdown>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(GetCurrentFractal());
+        }
+    }
+
+    public fractal GetCurrentFractal()
+    {
+        string currentSelection = drop.options[drop.value].text;
+        if (currentSelection == "Mandelbrot")
+            return fractal.mandlebrot;
+        if (currentSelection == "Julia Set")
+            return fractal.juliaSet;
+        else
+            return fractal.burningShip;
+    }
+}
+
+public enum fractal
+{
+    mandlebrot,
+    juliaSet,
+    burningShip
+}
